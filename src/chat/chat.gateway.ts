82951ba8069ca15b38f0c56d30c19802227f0535
anantasharma510 <anantasharma510@gmail.com>
2026-03-30 09:42:15 +0545

    feat: implement user join/leave and messaging logic in ChatGateway
    - track connected users with id and name
    - emit 'user-joined' and 'user-left' events
    - broadcast messages with username via 'conversation' event
    - update online users count when users disconnect
    - handle typed messages with typing stopped notification

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -10,16 +10,45 @@ import {
 import { ChatService } from './chat.service';
 import { Socket, Server } from 'socket.io';
 
+
+interface User {
+  id:string;
+  name:string;
+}
 @WebSocketGateway(4000, { cors: {} })
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
+  private users : User[]=[];
+
+
+
+
   handleConnection(client: Socket) {
+    const name = client.handshake.query.name as string || 'Unknown';
+    this.users.push({id:client.id,name});
+    client.broadcast.emit("user-joined", {id:client.id,name,message: `${name} has joined the chat`});
+
     console.log('Client connected:', client.id);
   }
 
 
   handleDisconnect(client: Socket) {
+    const obj =  this.users.find((u)=> u.id === client.id);
+    const index = this.users.findIndex((u)=> u.id === client.id);
+    client.broadcast.emit("user-left", `{obj?.name} has left the chat`);
+    if(index !== -1){
+      this.users.splice(index,1);
+    }
+    client.broadcast.emit("stopped-typing", this.users.length);
+    return true;
     console.log('Client disconnected:', client.id);
+  }
+
+
+  @SubscribeMessage('message')
+  handleEvent(@ConnectedSocket() client:Socket, @MessageBody() message: string) {
+    client.broadcast.emit("conversation",{name:this.users.find((u)=> u.id === client.id)?.name,message});
+    return message; 
   }
 
   // constructor(private readonly chatService: ChatService) {}
@@ -27,14 +56,14 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   // You can uncomment later when needed for rooms / global emit
   // @WebSocketServer() server: Server;
 
-  @SubscribeMessage('events')
-  handleEvent(
-    @ConnectedSocket() client: Socket,
-    @MessageBody() data: string,
-  ) {
-    console.log(data);
+  // @SubscribeMessage('events')
+  // handleEvent(
+  //   @ConnectedSocket() client: Socket,
+  //   @MessageBody() data: string,
+  // ) {
+  //   console.log(data);
 
-    // sends to everyone EXCEPT sender
-    client.broadcast.emit('events', data);
-  }
+  //   // sends to everyone EXCEPT sender
+  //   client.broadcast.emit('events', data);
+  // }
 }
